7916ee9c9d29ffcdaec3bc9f498414b5099573b8
spurtell <spurtell@redhat.com>
2014-07-21 10:28:15 -0400

    Initial Commit

---
+++ lib/_ext/pipeline.rb
@@ -0,0 +1,9 @@
+require 'bootstrap-sass'
+
+Awestruct::Extensions::Pipeline.new do
+  helper Awestruct::Extensions::Partial
+  # extension Awestruct::Extensions::Posts.new '/news'
+  # extension Awestruct::Extensions::Indexifier.new
+  # Indexifier *must* come before Atomizer
+  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+end
